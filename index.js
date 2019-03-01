c49844d5baa7dcc8ed5699159acaac3d08e5dde2
Oscar Garay <ojcgv21@gmail.com>
2019-03-02 00:20:49 +0100

    bye bye demo

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const sqlPool = require('./utils/standalone.js');
 const sqlTools = require('./utils/sql.js');
+const pedidos = require('./modules/pedidos.js');
 const ticketPrinter = require('./modules/ticket.printer.js');
 
 sqlTools.sw(async () => {
   const connection = await sqlPool.connection();
-  const query = 'SELECT * FROM venta_detalle';
-  const rows = await connection.query(query);
-  console.log(rows);
+  const ps = await pedidos.get(connection); 
+
+  const tickets = pedidos.buildTickets(ps);
+
+  sqlTools.pmap(tickets, async (ticket) => {
+    await ticketPrinter.sendPrintAsync(ticket);
+  }, { concurrency: 1 });
+  
   await connection.end();
 });
