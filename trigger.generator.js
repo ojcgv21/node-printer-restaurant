3a2e31db5bbffb414ac120c0f9d2f1fee22e57f4
Oscar Garay <ojcgv21@gmail.com>
2019-03-09 00:03:40 +0100

    update trigger

--- trigger.generator.js
+++ trigger.generator.js
@@ -4,9 +4,9 @@ const sqlTools = require('./utils/sql.js');
 sqlTools.sw(async () => {
     const connection = await sqlPool.connection();
     const query =   " CREATE TRIGGER trg_printer " + 
-                    " AFTER INSERT ON venta FOR EACH ROW " +
+                    " AFTER INSERT ON venta_detalle FOR EACH ROW " +
                     " BEGIN " +
-                    " INSERT INTO pid SET saleId = NEW.vId, status = 'AZE'; " +
+                    " INSERT INTO pid SET saleId = NEW.vId, status = 'WAIT'; " +
                     " END;";
     await connection.query(query);
     await connection.commit();
